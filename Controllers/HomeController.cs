100d5929cf9ebaec1e173a5a84399151bce8014d
fernandoespinosa <fernandoespinosa@fernandoespinosa.org>
2017-06-30 01:19:10 -0400

    init!

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace host.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("direct-stuff")]
+        public IActionResult DirectStuff()
+        {
+            return Ok("This is your stuff directly delivered to you...");
+        }
+
+        [HttpPost]
+        [Route("redirected-stuff")]
+        public IActionResult RedirectedStuff()
+        {
+            return Redirect("/stuff");
+        }
+
+        [HttpGet]
+        [Route("stuff")]
+        public IActionResult Stuff()
+        {
+            return Ok("Stuff...");
+        }
+    }
+}
